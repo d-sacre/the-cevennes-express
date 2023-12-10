2302833b7ef792bbd9eb5e6f1580e004442ca713
Daniel Sacré <daniel.sacre@gmx.de>
2023-12-10 21:22:31 +0100

    started to implement basic functionality of in-game Popup Menu

--- menus/inGame/inGame_popupMenu.gd
+++ menus/inGame/inGame_popupMenu.gd
@@ -10,55 +10,27 @@ var buttonText : Dictionary = {
 	"exit": "Exit to System"
 } 
 
-onready var audioManager : Node = get_parent().get_parent().get_node("audioManager")
-
-func _on_button_pressed(buttonContext, buttonId, buttonRef) -> void:
-	# play button sound only if corresponding bus audio level is above 0:
-	var _tmp_user_settings : Dictionary = {"volume":{"sfx": {"ui": 10}}} # just for testing; normally: userSettingsManager.get_user_settings()
-	if _tmp_user_settings["volume"]["sfx"]["ui"] > 0:
-		audioManager.play_sfx(["ui", "button", "pressed"]) # play the button pressed sound
-	
-#	# button pressed FSM
-	match buttonId:
-		"play":
-			# needs some delay so any audio can finish playing; perhaps similar solution to exit?
-			self.visible = false
-			get_tree().paused = false
-		
-		# "settings":
-		# 	settingsPopout.visible = not settingsPopout.visible
-		# 	creditsPopout.visible = false
-		"credits":
-			get_tree().change_scene("res://menus/mainMenu/mainMenu.tscn")
-		
-			
-		"exit":
-			get_tree().quit()
-
-func _on_button_entered_hover(buttonContext, buttonId, buttonRef) -> void:
-	var _tmp_user_settings : Dictionary = {"volume":{"sfx": {"ui": 10}}} # userSettingsManager.get_user_settings()
-	if _tmp_user_settings["volume"]["sfx"]["ui"] > 0:
-		audioManager.play_sfx(["ui", "button", "hover"])
-
-	if not buttonRef.disabled:
-		buttonRef.text = "«" + buttonText[buttonId] + "»"
-	
-func _on_button_exited_hover(buttonContext, buttonId, buttonRef) -> void:
-	if not buttonRef.disabled:
-		buttonRef.text = buttonText[buttonId]
-
 func _ready():
 	self.visible = false
-
-#	# connect the button signals
-#	for _source in BUTTON_SOURCES:
-#		var _sourceRef = get_node(_source)
-#
-#		for _signal in BUTTON_SIGNALS:
-#			_sourceRef.connect(_signal, self, "_on_"+_signal)
 	
+	if OS.has_feature("JavaScript"):
+		$VBoxContainer/exitButtonHBox.visible = false
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		self.visible = !self.visible
 		get_tree().paused = !get_tree().paused 
+
+
+func _on_resumeButton_pressed():
+	self.visible = false
+	get_tree().paused = false
+
+
+func _on_returnToMainMenuButton_pressed():
+	get_tree().paused = false
+	get_tree().change_scene("res://menus/mainMenu/mainMenu.tscn")
+
+
+func _on_exitButton_pressed():
+	get_tree().quit()
