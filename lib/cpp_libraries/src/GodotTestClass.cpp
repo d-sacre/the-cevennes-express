06101b5bc75fa2875fa2e5a9565d049b1ba44322
Milatur <alles.geheim_2@gmx.de>
2024-02-24 20:20:01 +0100

    rerolling of Namechange of GodotFunctions

--- lib/cpp_libraries/src/GodotTestClass.cpp
+++ lib/cpp_libraries/src/GodotTestClass.cpp
@@ -27,19 +27,19 @@ float GodotTestClass::Add(float first, float second)
 }
 
 /** GDNative Initialize **/
-extern "C" void GDN_EXPORT GodotGdnativeInit(godot_gdnative_init_options * options)
+extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options * options)
 {
 	godot::Godot::gdnative_init(options);
 }
 
 /** GDNative Terminate **/
-extern "C" void GDN_EXPORT GodotGdnativeTerminate(godot_gdnative_terminate_options * options)
+extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options * options)
 {
 	godot::Godot::gdnative_terminate(options);
 }
 
 /** NativeScript Initialize **/
-extern "C" void GDN_EXPORT GodotNativescriptInit(void * handle)
+extern "C" void GDN_EXPORT godot_nativescript_init(void * handle)
 {
 	godot::Godot::nativescript_init(handle);
 
